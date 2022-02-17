285abd5130a165f2fdce2849006adba058f1b985
Stefana <farcasus@yahoo.com>
2022-02-17 19:55:48 +0200

    feat: add crypto table

--- src/components/tables/Table.tsx
+++ src/components/tables/Table.tsx
@@ -1,5 +1,48 @@
-function Table() {
-  return <div>Table</div>;
+import { useEffect } from "react";
+import "../../assets/styles/Table/Table.css";
+interface ITable {
+  id: string;
+  name: string;
+  image: string;
+  symbol: string;
+  price: string;
+  current_price: string;
+  market_cap: string;
+  total_volume: string;
+  low_24h: string;
+}
+function Table(props: { data: any }) {
+  return (
+    <table className="table">
+      <thead className="table-head">
+        <tr className="table-head-row">
+          <th>NAME</th>
+          <th>PRICE</th>
+          <th>MARKET CAP</th>
+          <th>VOLUME 24H</th>
+          <th>LOW 24H </th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((crypto: ITable) => (
+          <tr className="table-body-row" key={crypto.id}>
+            <td className="table-body-name">
+              <img
+                className="table-body-symbol"
+                src={crypto.image}
+                alt="crypto symbol"
+              />
+              {crypto.name} {crypto.symbol}
+            </td>
+            <td>{crypto.current_price}</td>
+            <td>{crypto.market_cap}</td>
+            <td>{crypto.total_volume}</td>
+            <td>{crypto.low_24h}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,28 @@
 import TopNavigation from "../components/navigation/TopNavigation";
 import MainStatsBar from "../components/navigation/MainStatsBar";
 import Carousel from "../components/carousel/Carousel";
+import Table from "../components/tables/Table";
 import "../assets/styles/HomePage/HomePage.css";
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
+  const [data, setData] = useState<string[]>([]);
+  const URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+  useEffect(() => {
+    fetch(URL)
+      .then((date) => date.json())
+      .then(setData)
+      .catch((err) => {
+        console.error(err);
+      });
+    console.log(data);
+  }, []);
   return (
     <div className="home">
       <MainStatsBar />
       <TopNavigation />
       <Carousel />
+      <Table data={data} />
     </div>
   );
 }
