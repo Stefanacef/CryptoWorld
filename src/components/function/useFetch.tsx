58284e6e619c216efa37bac0c534ffecafb70616
Stefana <farcasus@yahoo.com>
2022-02-21 11:12:54 +0200

    feat: add custom hook, useFetch

---
+++ src/components/function/useFetch.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+interface IPost {
+  id: number;
+  content: string;
+}
+const useFetch = (url: string) => {
+  const [data, setData] = useState<IPost[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const abortCount = new AbortController();
+    fetch(url, { signal: abortCount.signal })
+      .then((respons) => {
+        if (!respons.ok) {
+          throw Error("could not fetch the data from that resource");
+        }
+        return respons.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        if (error.name === "AbortError") {
+          console.log("Error");
+        } else {
+          setError(error.message);
+          setIsLoading(false);
+        }
+      });
+    return () => abortCount.abort();
+  }, [url]);
+  return { data, isLoading, error };
+};
+
+export default useFetch;
--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -1,22 +1,14 @@
 import "../assets/styles/FeedPage/feed.css";
 import Post from "../components/posts/Post";
 import Textarea from "../components/textarea/Textarea";
-import { useEffect, useState } from "react";
+import useFetch from "../components/function/useFetch";
 interface IPost {
   id: number;
   content: string;
 }
 function FeedPage() {
-  const [data, setData] = useState<IPost[]>([]);
   const URL: string = `http://localhost:8000/posts`;
-  useEffect(() => {
-    fetch(URL)
-      .then((date) => date.json())
-      .then(setData)
-      .catch((err) => {
-        console.error(err);
-      });
-  }, [URL]);
+  const { data } = useFetch(URL);
   return (
     <div className="feed">
       <h1 className="feed-title">Feed</h1>
