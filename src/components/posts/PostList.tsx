e27c4c88dd1fe7a9f0647fe68abeb4bc6ae95179
Stefana <farcasus@yahoo.com>
2022-02-22 17:21:22 +0200

    feat: search posts by content

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -1,21 +1,45 @@
 import "../../assets/styles/Posts/Posts.css";
 import Post from "./Post";
 import { IPost } from "../../pages/FeedPage";
-import { useEffect } from "react";
+import { useState } from "react";
+import { AiOutlineSearch as Search } from "react-icons/ai";
 interface IPostList {
   setContentPost: React.Dispatch<React.SetStateAction<IPost[]>>;
   content: IPost[];
 }
 function PostList(props: IPostList) {
+  const [searchValue, setSearchValue] = useState<string>("");
+
   return (
     <div className="post-list">
-      {props.content.map((post) => (
-        <Post
-          key={post.id}
-          setContentPost={props.setContentPost}
-          content={post}
+      <div className="post-list-container">
+        <Search className="post-list-search-icon" />
+        <input
+          className="post-list-search"
+          type="text"
+          placeholder=" Search..."
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setSearchValue(e.target.value)
+          }
         />
-      ))}
+      </div>
+      {props.content
+        .filter((post) => {
+          if (searchValue === "") return post;
+          else if (
+            post.content
+              .toLocaleLowerCase()
+              .includes(searchValue.toLocaleLowerCase())
+          )
+            return post;
+        })
+        .map((post) => (
+          <Post
+            key={post.id}
+            setContentPost={props.setContentPost}
+            content={post}
+          />
+        ))}
     </div>
   );
 }
