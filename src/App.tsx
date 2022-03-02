1b7faed177df524d0d5ab623591a2aef5eb96fa7
Stefana <farcasus@yahoo.com>
2022-03-02 11:37:49 +0200

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import './App.css'
 import FeedPage from './pages/FeedPage'
 import HomePage from './pages/HomePage'
+import ExplorationPage from './pages/ExplorationPage'
+import Exchanges from './pages/Exchanges'
+import SignUpPage from './pages/SignUpPage'
 import { RecoilRoot } from 'recoil'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import MainStatsBar from './components/navigation/MainStatsBar'
+import TopNavigation from './components/navigation/TopNavigation'
 function App() {
   return (
     <div className="App">
-      <RecoilRoot>
-        <HomePage />
-        <FeedPage />
-      </RecoilRoot>
+      <Router>
+        <RecoilRoot>
+          <MainStatsBar />
+          <TopNavigation />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/feed" element={<FeedPage />} />
+            <Route path="/research" element={<ExplorationPage />} />
+            <Route path="/exchanges" element={<Exchanges />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Routes>
+        </RecoilRoot>
+      </Router>
     </div>
   )
 }
--- src/components/navigation/TopNavigation.tsx
+++ src/components/navigation/TopNavigation.tsx
@@ -1,21 +1,11 @@
 import '../../assets/styles/Navigation/Navigation.css'
+import NavigationLinks from './NavigationLinks'
 
 function TopNavigation() {
-  const links: string[] = ['Coins', 'Research', 'Exchanges', 'Feed']
-
   return (
     <nav className="navigation-top">
       <h3>LOGO</h3>
-      <div className="navigation-links">
-        {links.map((el, index) => (
-          <a href="https://crypto-world-stefana.netlify.app/" key={index}>
-            {el}
-          </a>
-        ))}
-        <button type="submit" className="navigation-button">
-          Sign up{' '}
-        </button>
-      </div>
+      <NavigationLinks />
     </nav>
   )
 }
