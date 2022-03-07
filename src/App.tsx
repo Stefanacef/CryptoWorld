2b5f32e2e66a0f57c9fda09bfffdb8ba8f624236
Stefana <farcasus@yahoo.com>
2022-03-07 10:32:02 +0200

    refactor: change route path

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import ExplorationPage from './pages/ExplorationPage'
 import Exchanges from './pages/Exchanges'
 import SignUpPage from './pages/SignUpPage'
 import { RecoilRoot } from 'recoil'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
 import MainStatsBar from './components/navigation/MainStatsBar'
 import TopNavigation from './components/navigation/TopNavigation'
 function App() {
@@ -16,7 +21,8 @@ function App() {
           <MainStatsBar />
           <TopNavigation />
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<Navigate replace to="/coins" />} />
+            <Route path="/coins" element={<HomePage />} />
             <Route path="/feed" element={<FeedPage />} />
             <Route path="/research" element={<ExplorationPage />} />
             <Route path="/exchanges" element={<Exchanges />} />
