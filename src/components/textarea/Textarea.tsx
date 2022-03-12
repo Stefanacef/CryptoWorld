dc7bb3829033d1caca5bb65076febc3778ca1931
Stefana <farcasus@yahoo.com>
2022-03-12 19:08:02 +0200

    feat: add validation textArea

--- src/components/textarea/Textarea.tsx
+++ src/components/textarea/Textarea.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
-import { FormattedMessage } from 'react-intl'
-import { TextareaAutosize, Button, Box } from '@mui/material'
+import React, { useMemo, useState } from 'react'
+import { FormattedMessage, useIntl } from 'react-intl'
+import { TextareaAutosize, Button, Box, Grid } from '@mui/material'
+import { Formik } from 'formik'
+import { IPost } from '../posts/types'
+import * as Yup from 'yup'
 
 export interface ITextarea {
   setContent: React.Dispatch<React.SetStateAction<string>>
@@ -8,33 +11,72 @@ export interface ITextarea {
 }
 
 function Textarea(props: ITextarea) {
-  const [content, setContent] = useState<string>('')
+  const intl = useIntl()
+  const [count, setCount] = useState(0)
+  const initialValue = useMemo<Partial<IPost>>(() => ({ content: '' }), [])
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    props.setContent(content)
-    setContent('')
+  const validationSchema = Yup.object({
+    content: Yup.string()
+      .min(3, 'feed.textarea.toShort')
+      .max(100, 'feed.textarea.toLong')
+      .required('feed.textarea.required'),
+  })
+
+  const handleSubmit = (values: Partial<IPost>) => {
+    values.content && props.setContent(values.content)
+  }
+
+  const getTranslate = (message: string) => {
+    return (
+      <FormattedMessage
+        id={message}
+        defaultMessage="Message need to be between 3 and 100 characters"
+      />
+    )
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Box>
-        <TextareaAutosize
-          maxRows={4}
-          value={content}
-          style={{ width: ' 100%', height: 100, padding: 20 }}
-          onChange={e => {
-            setContent(e.target.value)
-          }}
-          placeholder={props.placeholder}
-        />
-      </Box>
-      <Box mt="15px">
-        <Button variant="outlined" type="submit">
-          <FormattedMessage id="button.post" defaultMessage="Save" />
-        </Button>
-      </Box>
-    </form>
+    <Formik
+      onSubmit={(values, { resetForm }) => {
+        handleSubmit(values)
+        resetForm()
+        setCount(0)
+      }}
+      initialValues={initialValue}
+      validationSchema={validationSchema}
+    >
+      {({ values, setFieldValue, submitForm, errors, touched }) => (
+        <Box>
+          <TextareaAutosize
+            maxRows={4}
+            value={values.content}
+            style={{ width: ' 100%', height: 100, padding: 20 }}
+            onChange={e => {
+              setFieldValue('content', e.target.value)
+              setCount(e.target.value.length)
+            }}
+            placeholder={props.placeholder}
+          />
+          <Grid container justifyContent="space-between">
+            <Grid item>
+              {errors.content && touched.content ? (
+                <span style={{ color: '#FC4F4F' }}>
+                  {getTranslate(errors.content)}
+                </span>
+              ) : null}
+            </Grid>
+            <Grid item color="lightgrey">
+              {count}/100
+            </Grid>
+          </Grid>
+          <Box mt="15px">
+            <Button variant="outlined" onClick={() => submitForm()}>
+              <FormattedMessage id="button.post" defaultMessage="Save" />
+            </Button>
+          </Box>
+        </Box>
+      )}
+    </Formik>
   )
 }
 
