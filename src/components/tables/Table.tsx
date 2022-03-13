257d7dc8941ab0addc3664f7f15ecb2880786da0
Stefana <farcasus@yahoo.com>
2022-03-13 14:03:30 +0200

    feat: add sorting

--- src/components/tables/Table.tsx
+++ src/components/tables/Table.tsx
@@ -1,19 +1,20 @@
-// import { FormattedMessage } from 'react-intl'
 // import { Link } from 'react-router-dom'
 import '../../assets/styles/Table/Table.css'
-import { useTable } from 'react-table'
+import { useTable, useSortBy } from 'react-table'
 import { columnsData } from './Columns'
 import { useMemo } from 'react'
-
 function Table(props: { data: any }) {
   const data = useMemo(() => props.data, [props.data])
   const columns = useMemo(() => columnsData, [])
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
-      columns,
-      data: data,
-    })
+    useTable(
+      {
+        columns,
+        data: data,
+      },
+      useSortBy
+    )
 
   return (
     <>
@@ -22,7 +23,16 @@ function Table(props: { data: any }) {
           {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map(column => (
-                <th {...column.getFooterProps()}>{column.render('Header')}</th>
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
               ))}
             </tr>
           ))}
