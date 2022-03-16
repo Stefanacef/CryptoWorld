8bb1ef5eb90e09b5e38338ea82fa6e008c082130
Stefana <farcasus@yahoo.com>
2022-03-16 11:07:36 +0200

    feat: add Skeleton from mui

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,9 @@
 import Carousel from '../components/carousel/Carousel'
 import CoinCard from '../components/cards/CoinCard'
 import CoinsTable from '../components/tables/CoinsTable'
-import { Grid, Box, Card } from '@mui/material'
+import { Grid, Box, Card, Skeleton } from '@mui/material'
 import { useQuery } from 'react-query'
+import { FormattedMessage } from 'react-intl'
 
 interface ICryptoCoin {
   id: string
@@ -17,43 +18,61 @@ export default function HomePage() {
     const response = await fetch(URL)
     return response.json()
   }
-  const { data, isLoading, isError, error, isIdle } = useQuery<
-    ICryptoCoin[],
-    Error
-  >('coins', fetchCoins)
+  const { data, isLoading, isError, error } = useQuery<ICryptoCoin[], Error>(
+    'coins',
+    fetchCoins
+  )
 
   const topFiveCoins = data?.slice(-5)
 
   return (
     <Box m="30px">
-      {isIdle ? (
-        'Not ready...'
-      ) : isLoading ? (
-        <span>Loading...</span>
-      ) : isError ? (
-        <span>Error: {error?.message}</span>
+      {isError ? (
+        <span>
+          <FormattedMessage id="generic.label.error" defaultMessage="Error" />
+          {error?.message}
+        </span>
       ) : (
         <>
           <Card>
             <Box p="30px">
               <Carousel>
                 <Grid container columnGap="15px" rowGap="15px">
-                  {topFiveCoins?.map((crypto: ICryptoCoin) => (
-                    <Grid item key={crypto.id}>
-                      <CoinCard
-                        border=" border border-purple"
-                        title={crypto.name}
-                        icon={crypto.image}
-                        price={crypto.current_price}
-                      />
-                    </Grid>
-                  ))}
+                  {isLoading
+                    ? Array.from(new Array(5)).map((skeletonCoin, index) => (
+                        <Skeleton
+                          key={index}
+                          variant="rectangular"
+                          width={170}
+                          height={170}
+                          animation="wave"
+                        />
+                      ))
+                    : topFiveCoins?.map((crypto: ICryptoCoin) => (
+                        <Grid item key={crypto.id}>
+                          <CoinCard
+                            border=" border border-purple"
+                            title={crypto.name}
+                            icon={crypto.image}
+                            price={crypto.current_price}
+                          />
+                        </Grid>
+                      ))}
                 </Grid>
               </Carousel>
             </Box>
           </Card>
           <Box mt="30px">
-            <CoinsTable data={data} />
+            {isLoading ? (
+              <Skeleton
+                variant="rectangular"
+                width="100%"
+                height="500px"
+                animation="wave"
+              />
+            ) : (
+              <CoinsTable data={data} />
+            )}
           </Box>
         </>
       )}
