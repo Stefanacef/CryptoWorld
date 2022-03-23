ed0bcd7c89cbbb29eb868ea1d8a99e86ec6f6559
Stefana <farcasus@yahoo.com>
2022-03-23 10:18:21 +0200

    style: table headers

--- src/components/transactions/table/FilterByType.tsx
+++ src/components/transactions/table/FilterByType.tsx
@@ -1,4 +1,4 @@
-import { FormControl, MenuItem, Select } from '@mui/material'
+import { Box, FormControl, MenuItem, Select } from '@mui/material'
 import { useMemo } from 'react'
 import { FormattedMessage, useIntl } from 'react-intl'
 
@@ -15,37 +15,45 @@ const FilterByType = ({ column }: any) => {
   }, [intl])
 
   return (
-    <FormControl fullWidth>
-      <Select
-        labelId="type-selector-label"
-        id="type-selector"
-        onChange={e => {
-          setFilter(e.target.value || undefined)
-        }}
-        onClick={e => e.stopPropagation()}
-        value={filterValue || ''}
-        size="small"
-        sx={{ height: '30px' }}
-      >
-        <MenuItem value="">
-          <FormattedMessage id="transaction.all" defaultMessage="All" />
-        </MenuItem>
-        {options.map((option, i) => (
-          <MenuItem
-            key={i}
-            value={
-              option === 'Cumparat'
-                ? 'Buy'
-                : option === 'Vandut'
-                ? 'Sell'
-                : option
-            }
-          >
-            {option}
+    <Box
+      sx={{
+        position: 'absolute',
+        bottom: '10px',
+        width: '70%',
+      }}
+    >
+      <FormControl fullWidth>
+        <Select
+          labelId="type-selector-label"
+          id="type-selector"
+          onChange={e => {
+            setFilter(e.target.value || undefined)
+          }}
+          onClick={e => e.stopPropagation()}
+          value={filterValue || ''}
+          size="small"
+          sx={{ height: '30px' }}
+        >
+          <MenuItem value="">
+            <FormattedMessage id="transaction.all" defaultMessage="All" />
           </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+          {options.map((option, i) => (
+            <MenuItem
+              key={i}
+              value={
+                option === 'Cumparat'
+                  ? 'Buy'
+                  : option === 'Vandut'
+                  ? 'Sell'
+                  : option
+              }
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
   )
 }
 
